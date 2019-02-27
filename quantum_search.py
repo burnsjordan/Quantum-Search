7bba6371971406d5c0a39c00eecf6dea0ea1f8a4
Jordan Burns <burns.s.jordan@gmail.com>
2019-02-27 13:12:49 -0700

    Update create_hamiltonian to use parameters specified at beginning.

--- quantum_search.py
+++ quantum_search.py
@@ -1,10 +1,11 @@
 import numpy as np
-from scipy import linalg as sp
+from scipy import linalg as spl
+import scipy as sp
 
 # Adjustable Parameters
 alpha = 1/(2**(1/2)) # Proportion of target solutions in the initial state
-applications = 1 # Number of times to apply quantum search
-t = 1/(2**(1/2)) # Time to measure at
+applications = 10 # Number of times to apply quantum search
+t = sp.pi*(2**(1/2))/2 # Time to measure at
 a = 1.0 # Proportion of |x><x| in the hamiltonian
 b = 1.0 # Proportion of |psi><psi| in the hamiltonian
 c = 0 # Proportion of |x><psi| in the hamiltonian
@@ -26,7 +27,7 @@ def create_hamiltonian(a,b,c,d):
     H = np.add(H,c*np.kron(np.transpose(x),psi))
     H = np.add(H,d*np.kron(np.transpose(psi),x))
     H = H*(1/(np.linalg.det(H)**(1/2)))
-    H = sp.expm(1j*t*H)
+    H = spl.expm(-1j*t*H)
 
 # Function that applies quantum search using the specified hamiltonian
 def qsearch(input):
@@ -34,7 +35,7 @@ def qsearch(input):
     psi = np.dot(H,input)
 
 # Create the specified hamiltonian
-create_hamiltonian(1.0,1.0,0,0)
+create_hamiltonian(a,b,c,d)
 
 # Apply quantum search desired amount of times
 for i in range(applications):
